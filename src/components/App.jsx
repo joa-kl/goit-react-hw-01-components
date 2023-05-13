99ed594d4a05cbb82e5bdaa1cec6ed1c80bea4e2
Joanna K <joannakleinrm@gmail.com>
2023-05-13 18:39:30 +0200

    Profile created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
-import Profile from "./Profile";
+import { Profile } from '../components/Profile/Profile';
+import user from '../data/user.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
       
+    // </div>
+
+    <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import Profile from 'components/Profile';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Profile />
   </React.StrictMode>
 );
